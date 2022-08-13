2d7d15d05b958256858cc04cb262c901d49052f1
Xiaoyue Chen <xiaoyue.chen.0484@student.uu.se>
2022-08-13 22:37:44 +0200

    Add desktop environment

---
+++ denv.el
@@ -0,0 +1,152 @@
+;;; denv.el --- My desktop environment configurations  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Xiaoyue Chen
+
+;; Author: Xiaoyue Chen <xiaoyue.chen@it.uu.se>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(with-temp-buffer
+  (call-process "xrandr" nil t nil)
+  (goto-char (point-min))
+  (when (re-search-forward
+         "\n[^ ]+ connected\\(?: .*\\)? \\([[:digit:]]+\\)x\\([[:digit:]]+\\)"
+         nil t)
+    (let ((resx (string-to-number (match-string 1)))
+          (resy (string-to-number (match-string 2))))
+      (when (and (<= 3840 resx)
+                 (<= 2160 resy))
+        (setenv "GDK_SCALE" "2")
+        (setenv "QT_SCALE_FACTOR" "2")))))
+
+(add-to-list 'default-frame-alist
+             '(font . "Source Code Pro-16"))
+
+(call-process "xsetroot" nil nil nil "-cursor_name" "left_ptr")
+(call-process "xset" nil nil nil "r" "rate" "200" "60")
+(call-process "picom" nil nil nil "-b")
+(start-process "xscreensaver" nil "xscreensaver" "--no-splash")
+(start-process "nm-applet" nil "nm-applet")
+
+(setenv "VISUAL" "emacsclient")
+(setenv "EDITOR" (getenv "VISUAL"))
+
+(use-package server
+  :config
+  (unless (server-running-p) (server-start)))
+
+(use-package time
+  :init
+  (setq display-time-24hr-format t)
+  (setq display-time-default-load-average nil)
+  :config
+  (display-time-mode))
+
+(use-package battery
+  :config
+  (display-battery-mode))
+
+(use-package exwm
+  :init
+  (setq exwm-workspace-number 4)
+  (setq exwm-input-global-keys
+        `(([?\s-r] . exwm-reset)
+          ([?\s-w] . exwm-workspace-switch)
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda () (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))
+          ([?\s-&] . (lambda (command)
+		       (interactive (list (read-shell-command "$ ")))
+		       (start-process-shell-command command nil command)))
+          ([?\s-b] . switch-to-buffer)
+          ([?\s-o] . other-window)))
+  (setq exwm-input-simulation-keys
+        '(([?\C-b] . [left])
+          ([?\M-b] . [C-left])
+          ([?\C-f] . [right])
+          ([?\M-f] . [C-right])
+          ([?\C-p] . [up])
+          ([?\C-n] . [down])
+          ([?\C-a] . [home])
+          ([?\C-e] . [end])
+          ([?\M-<] . [home])
+          ([?\M->] . [end])
+          ([?\M-v] . [prior])
+          ([?\C-v] . [next])
+          ([?\C-d] . [delete])
+          ([?\M-d] . [C-delete])
+          ([?\C-k] . [S-end delete])
+          ([?\C-w] . [?\C-x])
+          ([?\M-w] . [?\C-c])
+          ([?\C-y] . [?\C-v])
+          ([?\C-s] . [?\C-f])))
+
+  :config
+  (use-package exwm-config
+    :config
+    (exwm-config-ido))
+
+  (use-package exwm-systemtray
+    :config
+    (exwm-systemtray-enable))
+
+  (use-package exwm-randr
+    :init
+    (setq exwm-randr-workspace-monitor-plist
+          '(0 "eDP-1-1"))
+    :config
+    (exwm-randr-enable))
+
+  (use-package desktop-environment
+    :init
+    (setq desktop-environment-screenlock-command "xscreensaver-command -lock")
+    :config
+    (define-key desktop-environment-mode-map
+      (kbd "<XF86KbdBrightnessUp>")
+      'desktop-environment-keyboard-backlight-increment)
+    (define-key desktop-environment-mode-map
+      (kbd "<XF86KbdBrightnessDown>")
+      'desktop-environment-keyboard-backlight-decrement)
+    (desktop-environment-mode))
+
+  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+
+  (add-hook 'exwm-update-class-hook
+            (lambda ()
+              (exwm-workspace-rename-buffer exwm-class-name)))
+
+  (exwm-enable))
+
+(use-package ednc
+  :demand
+  :config
+  (defun list-notifications ()
+    (mapconcat 'ednc-format-notification (ednc-notifications) ""))
+  (nconc global-mode-string '((:eval (list-notifications))))
+  (add-hook 'ednc-notification-presentation-functions
+            (lambda (&rest _) (force-mode-line-update t)))
+  (ednc-mode)
+  :bind
+  (("C-c n" . (lambda () (interactive)
+                (switch-to-buffer ednc-log-name)))))
+
+;;; desktop.el ends here
--- init.el
+++ init.el
@@ -32,7 +32,6 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 (setq use-package-hook-name-suffix nil)
-
 (setq x-underline-at-descent-line t)
 (setq enable-recursive-minibuffers t)
 (setq read-file-name-completion-ignore-case t
@@ -41,7 +40,7 @@
 (setq sentence-end-double-space nil)
 (setq require-final-newline t)
 (setq-default indent-tabs-mode nil)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq delete-by-moving-to-trash t)
 
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
@@ -49,40 +48,57 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
-;; (defun enable-dsktenv (_)
-;;   (load "~/.emacs.d/dsktenv.el"))
+(use-package doom-modeline
+  :config (doom-modeline-mode))
 
-;; (defun my-cmd-args ()
-;;   (add-to-list 'command-switch-alist '("--dsktenv" . enable-dsktenv)))
+(use-package bash-completion
+    :hook
+    (shell-dynamic-complete-functions . bash-completion-dynamic-complete))
+
+(use-package savehist
+  :config
+  (savehist-mode))
+
+(use-package which-key
+    :config
+    (which-key-mode))
 
 (use-package menu-bar
-  :init
-  (menu-bar-mode 0))
+  :config
+  (menu-bar-mode -1))
 
 (use-package tool-bar
+  :config
+  (tool-bar-mode -1))
+
+(use-package scroll-bar
+  :config
+  (scroll-bar-mode -1))
+
+(use-package ido
   :init
-  (tool-bar-mode 0))
+  (setq ido-use-filename-at-point 'guess)
+  (setq ido-use-url-at-point t)
+  :config
+  (ido-mode))
+
+(use-package simple
+  :hook
+  (before-save-hook . delete-trailing-whitespace))
 
 (use-package paren
   :init
   (setq show-paren-delay 0)
+  :config
   (show-paren-mode))
 
 (use-package elec-pair
-  :init
+  :config
   (electric-pair-mode))
 
 (use-package delsel
-  :init
+  :config
   (delete-selection-mode))
-
-(use-package savehist
-  :init
-  (savehist-mode))
-
-(use-package ffap
-  :init
-  (ffap-bindings))
 
 (use-package locate
   :bind
@@ -105,7 +121,8 @@
   :init
   (setq imenu-auto-rescan t)
   :bind
-  (("C-c i" . imenu)))
+  (:map prog-mode-map
+        ("C-c i" . imenu)))
 
 (use-package compile
   :bind
@@ -121,12 +138,8 @@
   :init
   (setq gdb-many-windows t))
 
-(use-package which-key
-  :init
-  (which-key-mode))
-
 (use-package autorevert
-  :init
+  :config
   (global-auto-revert-mode))
 
 (use-package comint
@@ -142,8 +155,9 @@
          '("\\.\\([Hh]\\|hh\\|hpp\\|hxx\\|h\\+\\+\\)\\'" . "C / C++ header"))
         (c-source-condition
          '("\\.\\([Cc]\\|cc\\|cpp\\|cxx\\|c\\+\\+\\)\\'" . "C / C++ program")))
-    (define-auto-insert c-header-condition 'insert-lice)
-    (define-auto-insert c-source-condition 'insert-lice))
+    (define-auto-insert c-header-condition insert-lice)
+    (define-auto-insert c-source-condition insert-lice))
+  :config
   (auto-insert-mode))
 
 (use-package copyright
@@ -164,10 +178,6 @@
    '((shell . t)))
   :bind
   (("C-c l" . org-store-link)))
-
-(use-package bash-completion
-  :hook
-  (shell-dynamic-complete-functions . bash-completion-dynamic-complete))
 
 (use-package tramp
   :config
@@ -202,8 +212,6 @@
   :config
   (eldoc-add-command 'c-electric-paren))
 
-(use-package magit)
-
 (use-package company
   :init
   (setq company-minimum-prefix-length 3
@@ -213,6 +221,7 @@
   (latex-mode-hook . company-mode))
 
 (use-package yasnippet
+  :defer t
   :config
   (yas-reload-all))
 
@@ -229,17 +238,18 @@
         ("C-c f" . eglot-format)
         ("C-c a" . eglot-code-actions))
   :config
-  (add-to-list 'eglot-server-programs '(cmake-mode . ("cmake-language-server")))
-  (cl-flet ((my-jdtls-contact (interactive)
-                              (let ((workspace
-                                     (expand-file-name
-                                      (md5 (project-root (project-current)))
-                                      "/tmp")))
-                                (list "jdtls" "-data" workspace))))
-    (add-to-list 'eglot-server-programs '(java-mode . my-jdtls-contact)))
+  (add-to-list 'eglot-server-programs
+               '(cmake-mode . ("cmake-language-server")))
+  (add-to-list 'eglot-server-programs
+               '(java-mode . (lambda () (interactive)
+                               (let ((workspace
+                                      (expand-file-name
+                                       (md5 (project-root (project-current)))
+                                       "/tmp")))
+                                 (list "jdtls" "-data" workspace)))))
   :hook
   ((c-mode-hook c++-mode-hook java-mode-hook python-mode-hook haskell-mode-hook)
-   . (eglot-ensure)))
+   . eglot-ensure))
 
 (use-package text-mode
   :config
@@ -252,6 +262,7 @@
     (bind-key "C-c d" 'disaster map))
   :hook
   (c-mode-common-hook . (lambda ()
+                          (setq-default fill-column 80)
                           (c-toggle-electric-state 1)
                           (c-toggle-comment-style 1)))
   (minizinc-mode-hook . (lambda ()
@@ -301,5 +312,7 @@
                        (TeX-fold-mode)
                        (TeX-source-correlate-mode))))
 
+(add-to-list 'command-switch-alist
+             '("--denv" . (lambda (_) (load "~/.emacs.d/denv.el"))))
 
 ;;; init.el ends here
